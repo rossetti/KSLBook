f18795005b2b4adf1801b194599f3d7904091676
Manuel Rossetti <rossetti@uark.edu>
2025-09-18 15:39:43 -0500

    fix commit

--- chapterFiles/Appendix-Distribution Fitting/Example2.R
+++
@@ -1,9 +0,0 @@
-myFile = file.choose() #example6-2Data.txt
-packageCnt = scan(myFile)
-hist(packageCnt, main="Packages per Shipment", xlab="#packages")
-stripchart(packageCnt,method="stack",pch="o")
-table(packageCnt)
-summary(packageCnt)
-tp = table(packageCnt)
-tp/40
-
